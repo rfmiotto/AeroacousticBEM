80f280de0786b7c123751e2b185bf03018b73ce6
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2025-07-14 17:05:36 -0300

    refactor: remove commented out code in MathUtils

--- src/foundation/utils/MathUtils.cpp
+++ src/foundation/utils/MathUtils.cpp
@@ -1,8 +1,5 @@
 #include "foundation/utils/MathUtils.hpp"
 #include <cmath> // For std::sqrt, std::pow, std::atan2, std::abs, std::cyl_bessel_j, std::cyl_neumann
-#include <numeric> // For std::inner_product (potentially, or manual dot product)
-#include <stdexcept> // For std::invalid_argument
-#include <vector>    // For std::vector
 
 namespace bem::foundation::utils::MathUtils {
 
@@ -38,35 +35,4 @@ double phase(types::Complex z) {
   return std::arg(z);
 }
 
-// std::pair<std::vector<double>, std::vector<double>>
-// gaussLegendreQuadrature(int n_points) {
-//   if (n_points == 2) {
-//     double point = std::numbers::inv_sqrt3;
-//     std::vector<double> points = {-point, point};
-//     std::vector<double> weights = {1.0, 1.0};
-//     return {points, weights};
-//   } else {
-//     throw std::invalid_argument("gaussLegendreQuadrature is currently only "
-//                                 "implemented for n_points = 2.");
-//   }
-// }
-//
-// std::pair<types::Point2D, double>
-// lineIntegrationTransform(const types::Point2D &start, const types::Point2D
-// &end,
-//                          double xi) {
-//   double common_factor_start = (1.0 - xi) / 2.0;
-//   double common_factor_end = (1.0 + xi) / 2.0;
-//
-//   types::Point2D transformed_point;
-//   transformed_point.x =
-//       common_factor_start * start.x + common_factor_end * end.x;
-//   transformed_point.y =
-//       common_factor_start * start.y + common_factor_end * end.y;
-//
-//   double jacobian = distance(start, end) / 2.0;
-//
-//   return {transformed_point, jacobian};
-// }
-//
 } // namespace bem::foundation::utils::MathUtils
--- src/foundation/utils/MathUtils.hpp
+++ src/foundation/utils/MathUtils.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <cmath>
-#include <utility> // For std::pair
-#include <vector>
 
 #include "foundation/types/MathObjectTypes.hpp"
 #include "foundation/utils/Constants.hpp"
@@ -19,18 +17,5 @@ bool isZero(double value, double tol = MACHINE_EPSILON);
 bool isEqual(double v1, double v2, double tol = MACHINE_EPSILON);
 types::Real magnitude(types::Complex z);
 types::Real phase(types::Complex z);
-// types::Complex hankelFirstKind(int order, double z);
-// types::Complex greensFunction2D(const types::Point2D &source,
-//                                 const types::Point2D &field, double k);
-// types::Complex greensFunctionNormalDerivative2D(const types::Point2D &source,
-//                                                 const types::Point2D &field,
-//                                                 const types::Point2D &normal,
-//                                                 double k);
-// std::pair<std::vector<double>, std::vector<double>>
-// gaussLegendreQuadrature(int n_points);
-// std::pair<types::Point2D, double>
-// lineIntegrationTransform(const types::Point2D &start, const types::Point2D
-// &end,
-//                          double xi);
 
 } // namespace bem::foundation::utils::MathUtils
